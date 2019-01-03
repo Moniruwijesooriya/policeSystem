92583ccbbd1160e106f4a318c1828c8a66d4659f
sajith1028 <path.iceman@gmail.com>
2019-01-03 17:18:32 +0530

    Added background

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\DB;
 @section('content')
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
